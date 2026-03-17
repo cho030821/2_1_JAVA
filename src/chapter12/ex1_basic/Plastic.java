4c00da4f6a8fed4e29bb55e208bdfe2cc07af8c8
cho030821 <35185942+cho030821@users.noreply.github.com>
2026-03-17 11:58:42 +0900

    3

---
+++ src/chapter12/ex1_basic/Plastic.java
@@ -0,0 +1,14 @@
+package chapter12.ex1_basic;
+
+public class Plastic {
+    public void doPrinting() {
+
+        System.out.println("플라스틱 재료로 ㅊㄹ략");
+    }
+    @Override
+            public String toString(){
+            return("재료는 플라스틱");
+        }
+
+    }
+
---
+++ src/chapter12/ex1_basic/Powder.java
@@ -0,0 +1,14 @@
+package chapter12.ex1_basic;
+
+public class Powder {
+    public void doPrinting() {
+
+        System.out.println("밀가루 재료로 ㅊㄹ략");
+    }
+    @Override
+    public String toString(){
+        return("재료는 밀가루");
+    }
+
+}
+
