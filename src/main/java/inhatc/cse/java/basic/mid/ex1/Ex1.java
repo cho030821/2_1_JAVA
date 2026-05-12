ac20acef05f21966bc2d9b9bd350611aa5469be0
cho030821 <35185942+cho030821@users.noreply.github.com>
2026-05-12 11:51:58 +0900

    0512

--- src/main/java/inhatc/cse/java/basic/mid/ex1/Ex1.java
+++ src/main/java/inhatc/cse/java/basic/mid/ex1/Ex1.java
@@ -2,8 +2,7 @@ package main.java.inhatc.cse.java.basic.mid.ex1;
 
 public class Ex1 {
     static void main(String[] args) {
-
-        int amount =120000;
+        int amount = 120000;
         Level gold = Level.Gold;
         int point = (int)(amount * gold.getRate());
         System.out.println("적립 포인트 : " + point + "원");
